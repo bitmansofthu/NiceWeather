84e7fadaeb6dafedb8e3c76ba40f9b85757e37cf
AquaJava <aquajava@AquaJavas-MacBook-Pro.local>
2016-05-15 21:02:37 +0200

    Added unit test for JSON parser

--- TWCWeatherTests/TWCWeatherTests.m
+++ TWCWeatherTests/TWCWeatherTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "OpenWeatherMapParser.h"
+#import "WeatherInfo.h"
 
 @interface TWCWeatherTests : XCTestCase
 
@@ -24,16 +26,16 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testJsonParser {
+    NSString* json = @"{\"coord\":{\"lon\":-0.13,\"lat\":51.51},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"02d\"}],\"base\":\"cmc stations\",\"main\":{\"temp\":15.2,\"pressure\":1023.87,\"humidity\":52,\"temp_min\":15.2,\"temp_max\":15.2,\"sea_level\":1033.82,\"grnd_level\":1023.87},\"wind\":{\"speed\":4.16,\"deg\":324.504},\"clouds\":{\"all\":8},\"dt\":1463335880,\"sys\":{\"message\":0.0052,\"country\":\"GB\",\"sunrise\":1463285224,\"sunset\":1463341660},\"id\":2643743,\"name\":\"London\",\"cod\":200}";
+    
+    OpenWeatherMapParser* parser = [[OpenWeatherMapParser alloc] init];
+    WeatherInfo* info = [parser parseJsonData:[json dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    XCTAssert(parser.parseError == nil);
+    XCTAssert(info.temp != nil && [info.temp floatValue] == 15.2f);
+    XCTAssert(info.cloudiness != nil && [info.cloudiness integerValue] == 8);
+    
 }
 
 @end
